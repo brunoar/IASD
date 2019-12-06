dcadb38813f0134b20db8c3bd24e4a975f7194bb
Bruno Ribeiro <brunoamdr@hotmail.com>
2019-12-06 11:39:45 +0000

    second commit

---
+++ project2/main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3.6
+
+from aimapython.probability import elimination_ask
+from aimapython.probability import BayesNet
+import sys
+
+
+class Problem:
+
+    def __init__(self, fh):
+        # Place here your code to load problem from opened file object fh
+        # and use probability.BayesNet() to create the Bayesian network
+
+        lines = fh.readlines()
+        [R, C, S, P, M] = process(lines)
+
+        print(R)
+        print(C)
+        print(S)
+        print(P)
+        print(M)
+                
+    def solve(self):
+        # Place here your code to determine the maximum likelihood solution
+        # returning the solution room name and likelihood
+        # use probability.elimination_ask() to perform probabilistic inference
+        #return (room, likelihood)
+        pass
+
+
+def process(lines):
+    R=[]
+    C=[]
+    S=[]
+    P=[]
+    M=[]
+
+    for ln in lines:
+
+            if ln[0] == 'R':
+                R.append([s for s in ln.split()[1:] ])
+
+            if ln[0] == 'C':
+                doors = ln.split()[1:]
+                for door in doors:
+                    aux = []
+                    for i in range(len(door)):
+                        if door[i] == ',':
+                            aux.append(door[0:i])
+                            aux.append(door[i+1:])
+                    C.append(aux)
+
+            if ln[0] == 'S':
+                sensors = ln.split()
+                for s in sensors[1:]:
+                    j=0
+                    aux = []
+                    for i in range(len(s)):
+                        if s[i] == ':':
+                            aux.append(s[j:i])
+                            j=i+1
+                    aux.append(s[j:-1])
+                    S.append(aux)
+
+            if ln[0] == 'P':
+                P.append([s for s in ln.split()[1:] ])
+
+            if ln[0] == 'M':
+                measures = ln.split()[1:]
+                sampling = []
+                for m in measures:
+                    aux = []
+                    for i in range(len(m)):
+                        if m[i] == ':':
+                            aux.append(m[0:i])
+                            aux.append(m[i+1:])
+                    sampling.append(aux)
+                M.append(sampling)
+
+    return [R, C, S, P, M]
+
+
+def main():
+
+    if len(sys.argv) > 1:
+        fh = open(sys.argv[1])
+        prob = Problem(fh)
+        answer = prob.solve()
+    else:
+        print("Usage: %s <filename>" % (sys.argv[0]))
+
+
+main()
